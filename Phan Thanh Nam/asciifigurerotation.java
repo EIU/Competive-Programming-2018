e4ce6bbe71715bfc65dca055c7b83a22abd14d9c
Phan Thanh Nam <nam.thanhphan77@gmail.com>
2018-07-09 14:39:54 +0700

    update

--- Phan Thanh Nam/asciifigurerotation.java
+++ Phan Thanh Nam/asciifigurerotation.java
@@ -8,58 +8,61 @@ import java.util.StringTokenizer;
 
 public class Asciifigurerotation {
 	public static void main(String[] args) throws IOException {
-        InputReader sc = new InputReader(System.in);
-        boolean first = true;
-        while (true) {
-            int n = sc.nextInt();
-            if (n == 0) {
-                break;
-            }
-            if(first==true)System.out.println();
-            char[][] character = new char[n][n];
-            int length_max = 0;
-            for (int i = 0; i < n; i++) {
-                String s = sc.nextLine();
-                length_max=Math.max(length_max, s.length());
-                for (int j = s.length() - 1; j < 20; j++) {
-                    s += " ";
-                }
-                character[i] = s.toCharArray();
-            }
-            String[] ch = new String[n];
-            for (int i = 0; i < length_max; i++) {
-                for (int j = n - 1; j >= 0; j--) {
-                    char a = character[j][i];
-                    if (a == '|') {
-                        ch[j] = "-";
-                    } else if (a == '-') {
-                        ch[j] = "|";
-                    } else if (a == ' ') {
-                        ch[j] = " ";
-                    }  else {
-                        ch[j] = a + "";
-                    }
-                }
-                TrimRight(ch);
-                for (int j = n - 1; j >= 0; j--) {
-                    if (ch[j].equals(" ")) {
-                        System.out.print(" ");
-                    } else {
-                        System.out.print(ch[j]);
-                    }
-                }
-                System.out.println();
-            }
-        }
+		InputReader sc = new InputReader(System.in);
+		boolean first = true;
+		while (true) {
+			int n = sc.nextInt();
+			if (n == 0) {
+				break;
+			}
+			if (first == true)
+				System.out.println();
+			first = false;
+			char[][] character = new char[n][n];
+			int length_max = 0;
+			for (int i = 0; i < n; i++) {
+				String s = sc.nextLine();
+				length_max = Math.max(length_max, s.length());
+				for (int j = s.length() - 1; j < 20; j++) {
+					s += " ";
+				}
+				character[i] = s.toCharArray();
+			}
+			String[] ch = new String[n];
+			for (int i = 0; i < length_max; i++) {
+				for (int j = n - 1; j >= 0; j--) {
+					char a = character[j][i];
+					if (a == '|') {
+						ch[j] = "-";
+					} else if (a == '-') {
+						ch[j] = "|";
+					} else if (a == ' ') {
+						ch[j] = " ";
+					} else {
+						ch[j] = a + "";
+					}
+				}
+				TrimRight(ch);
+				for (int j = n - 1; j >= 0; j--) {
+					if (ch[j].equals(" ")) {
+						System.out.print(" ");
+					} else {
+						System.out.print(ch[j]);
+					}
+				}
+				System.out.println();
+			}
+		}
 	}
+
 	public static String[] TrimRight(String[] s) {
 		for (int j = 0; j < s.length; j++) {
-            if (s[j] == " ") {
-                s[j] = "";
-            } else {
-                break;
-            }
-        }
+			if (s[j] == " ") {
+				s[j] = "";
+			} else {
+				break;
+			}
+		}
 		return s;
 	}
 
