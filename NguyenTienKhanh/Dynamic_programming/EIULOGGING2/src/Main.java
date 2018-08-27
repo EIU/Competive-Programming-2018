0fac63a0de2a9ef596faa5d1a93512b40e03541c
KhanhNguyeneiu1996 <38639969+KhanhNguyeneiu1996@users.noreply.github.com>
2018-08-27 09:37:15 +0700

    Upload
    Update

--- NguyenTienKhanh/Dynamic_programming/EIULOGGING2/src/Main.java
+++ NguyenTienKhanh/Dynamic_programming/EIULOGGING2/src/Main.java
@@ -1,120 +1,95 @@
-
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.Collections;
+ 
 class Main {
-
+ 
     public static InputReader input;
-
+    public static int[] level;
+    public static int[] pre;
+ 
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         input = new InputReader();
         int N = input.nextInt();
         long[] price = new long[N];
-        int[] count = new int[N];
-        Arrays.fill(count, 1);
-        for (int i = 0; i < N; i++) {
-            long T = input.nextLong();
-            price[i] = T;
+        for(int i=0;i<N;i++){
+            long T=input.nextLong();
+            price[i]=T;
         }
-        if (N > 2) {
-            long pri = price[2];
-            price[2] = Math.max(price[0], Math.max(price[2], price[2] + price[0]));
-             if (pri + price[0] == pri || price[0] == Math.max(pri, pri + price[0]) ) {
-                count[2] += count[0]+1;
-            }else{
-            if (pri + price[0] == pri || price[0] == Math.max(pri, pri + price[0]) ) {
-                count[2] += count[0];
-            }
-             }
-                
+        if(N>3)
+            price[3]=Math.max(price[0], Math.max(price[3], price[3]+price[0]));
+        if(N>4){
+            price[4]=Math.max(price[1], Math.max(price[4], Math.max(price[4]+price[1], price[4]+price[0])));
+        price[4]=Math.max(price[4], price[0]);
         }
-        int i;
-        for(i = 3;i<N;i++){
-            long pri=price[i];
-                 price[i]=Math.max(price[i-3], Math.max(price[i], Math.max(price[i]+price[i-2], price[i]+price[i-3])));
-                 price[i]=Math.max(price[i], price[i-2]);
-                 if(price[i]==pri){
-                     count[i]= pri==pri+price[i-2]&&pri==pri+price[i-3]?count[i]+count[i-2]+count[i-3]: pri==pri+price[i-2]?count[i]+count[i-2]: pri==pri+price[i-3]?count[i]+count[i-3]:count[i]; 
-                 }else if(price[i]==price[i-2]){
-                     count[i]= price[i-2]==price[i-3]&& price[i-2]==price[i-2]+pri?count[i]+count[i-2]+count[i-3]:price[i-2]==price[i-3]?count[i-3]+count[i-2]:price[i-2]==price[i-2]+pri?count[i]+count[i-2]:count[i-2];
-                     }else if(price[i]==price[i-3]){
-                     count[i]=price[i-3]==price[i-3]+pri?count[i]+count[i-3]:count[i-3];
-                 }else if(price[i]==pri+price[i-2]){
-                     count[i]=  price[i-2]==price[i-3]?count[i-2]+count[i-3]:count[i-2];
-                     
-                 }else
-                     count[i]=count[i-3];
-                 count[i]%=1000000007;
-                 
+        
+        for(int i = 5;i<N;i++){
+                 long pri= price[i];
+                 price[i]=Math.max(price[i], Math.max(pri+price[i-3], pri+price[i-4]));
+                 price[i]=Math.max(price[i-3], Math.max(price[i], pri+price[i-5]));
+                 price[i]=Math.max(price[i-4], price[i]);
+                 price[i]=Math.max(price[i-5], price[i]);
         }
-
-        long max = Long.MIN_VALUE;
-        long Count = 0;
-        for (i = price.length - 1; i > price.length - 3; i--) {
-            if (price[i] > max) {
-                max = price[i];
-                Count = count[i];
-            } else if (price[i] == max) {
-                Count += count[i];
-            }
+        long max=0;
+        for(int i=0;i<price.length;i++){
+            if(price[i]>max)
+                max=price[i];
         }
-        if (max < 0) {
-            Count = 1;
-            max = 0;
-        }
-
-        System.out.println(max + " " + Count);
+ 
+        System.out.println(max);
     }
-
+ 
     static class InputReader {
-
+ 
         InputStream is = System.in;
         byte[] inbuf = new byte[1 << 23];
         int lenbuf = 0, ptrbuf = 0;
-
+ 
         public InputReader() throws IOException {
             lenbuf = is.read(inbuf);
         }
-
+ 
         public int readByte() {
-
+ 
             if (ptrbuf >= lenbuf) {
                 return -1;
             }
-
+ 
             return inbuf[ptrbuf++];
         }
-
+ 
         public boolean hasNext() {
             int t = skip();
-
+ 
             if (t == -1) {
                 return false;
             }
             ptrbuf--;
             return true;
         }
-
+ 
         public boolean isSpaceChar(int c) {
             return !(c >= 33 && c <= 126);
         }
-
+ 
         public int skip() {
             int b;
             while ((b = readByte()) != -1 && isSpaceChar(b))
                 ;
             return b;
         }
-
+ 
         public double nextDouble() {
             return Double.parseDouble(next());
         }
-
+ 
         public char nextChar() {
             return (char) skip();
         }
-
+ 
         public String next() {
             int b = skip();
             StringBuilder sb = new StringBuilder();
@@ -124,7 +99,7 @@ class Main {
             }
             return sb.toString();
         }
-
+ 
         public char[] ns(int n) {
             char[] buf = new char[n];
             int b = skip(), p = 0;
@@ -134,7 +109,7 @@ class Main {
             }
             return n == p ? buf : Arrays.copyOf(buf, p);
         }
-
+ 
         public char[][] nm(int n, int m) {
             char[][] map = new char[n][];
             for (int i = 0; i < n; i++) {
@@ -142,7 +117,7 @@ class Main {
             }
             return map;
         }
-
+ 
         public int[] na(int n) {
             int[] a = new int[n];
             for (int i = 0; i < n; i++) {
@@ -150,7 +125,7 @@ class Main {
             }
             return a;
         }
-
+ 
         public int nextInt() {
             int num = 0, b;
             boolean minus = false;
@@ -160,7 +135,7 @@ class Main {
                 minus = true;
                 b = readByte();
             }
-
+ 
             while (true) {
                 if (b >= '0' && b <= '9') {
                     num = num * 10 + (b - '0');
@@ -170,7 +145,7 @@ class Main {
                 b = readByte();
             }
         }
-
+ 
         public long nextLong() {
             long num = 0;
             int b;
@@ -181,7 +156,7 @@ class Main {
                 minus = true;
                 b = readByte();
             }
-
+ 
             while (true) {
                 if (b >= '0' && b <= '9') {
                     num = num * 10 + (b - '0');
@@ -191,6 +166,7 @@ class Main {
                 b = readByte();
             }
         }
-
+ 
     }
 }
+ 
