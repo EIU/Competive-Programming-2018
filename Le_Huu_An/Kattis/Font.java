64822e84cbb08a8fc766eea973142bc6499c7772
Le Huu An <anhan8910@gmail.com>
2018-09-14 13:10:53 +0700

    update

---
+++ Le_Huu_An/Kattis/Font.java
@@ -0,0 +1,168 @@
+import java.io.*;
+import java.util.*;
+
+public class Font {
+	static InputReader reader;
+	static int n;
+
+	public static void main(String[] args) throws IOException {
+		reader = new InputReader();
+		n = reader.nextInt();
+		int[] word = new int[n];
+		for (int i = 0; i < 9; i++) {
+			int sum = 0;
+			String line = reader.next();
+			for (char j : line.toCharArray()) {
+				sum |= 1 << (j - 'a');
+			}
+			word[i] = sum;
+		}
+		System.out.println(solve(0, 0, n, word));
+	}
+
+	static int target = (1 << 26) - 1;
+
+	static int solve(int pos, int val, int n, int[] arr) {
+		if (pos == n) {
+			if (val == target) {
+				return 1;
+			}
+			return 0;
+		}
+
+		int ans = 0;
+
+		ans += solve(pos + 1, val | arr[pos], n, arr);
+		ans += solve(pos + 1, val, n, arr);
+
+		return ans;
+	}
+
+	static class InputReader {
+
+		InputStream is = System.in;
+		byte[] inbuf = new byte[1 << 24];
+		int lenbuf = 0, ptrbuf = 0;
+
+		public InputReader() throws IOException {
+			lenbuf = is.read(inbuf);
+		}
+
+		public int readByte() {
+
+			if (ptrbuf >= lenbuf) {
+				return -1;
+			}
+
+			return inbuf[ptrbuf++];
+		}
+
+		public boolean hasNext() {
+			int t = skip();
+
+			if (t == -1) {
+				return false;
+			}
+			ptrbuf--;
+			return true;
+		}
+
+		public boolean isSpaceChar(int c) {
+			return !(c >= 33 && c <= 126);
+		}
+
+		public int skip() {
+			int b;
+			while ((b = readByte()) != -1 && isSpaceChar(b))
+				;
+			return b;
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		public char nextChar() {
+			return (char) skip();
+		}
+
+		public String next() {
+			int b = skip();
+			StringBuilder sb = new StringBuilder();
+			while (!(isSpaceChar(b))) {
+				sb.appendCodePoint(b);
+				b = readByte();
+			}
+			return sb.toString();
+		}
+
+		public char[] ns(int n) {
+			char[] buf = new char[n];
+			int b = skip(), p = 0;
+			while (p < n && !(isSpaceChar(b))) {
+				buf[p++] = (char) b;
+				b = readByte();
+			}
+			return n == p ? buf : Arrays.copyOf(buf, p);
+		}
+
+		public char[][] nm(int n, int m) {
+			char[][] map = new char[n][];
+			for (int i = 0; i < n; i++) {
+				map[i] = ns(m);
+			}
+			return map;
+		}
+
+		public int[] na(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+
+		public int nextInt() {
+			int num = 0, b;
+			boolean minus = false;
+			while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+				;
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+
+			while (true) {
+				if (b >= '0' && b <= '9') {
+					num = num * 10 + (b - '0');
+				} else {
+					return minus ? -num : num;
+				}
+				b = readByte();
+			}
+		}
+
+		public long nextLong() {
+			long num = 0;
+			int b;
+			boolean minus = false;
+			while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+				;
+			if (b == '-') {
+				minus = true;
+				b = readByte();
+			}
+
+			while (true) {
+				if (b >= '0' && b <= '9') {
+					num = num * 10 + (b - '0');
+				} else {
+					return minus ? -num : num;
+				}
+				b = readByte();
+			}
+		}
+
+	}
+
+}
